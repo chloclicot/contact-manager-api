6feb2cab84c1a3ba92335fc5337b0390b5490e13
chloclicot <chloe.v.33@outlook.fr>
2023-08-26 02:22:00 +0200

    initial commit

---
+++ src/test/java/com/example/contactmanagerapi/ContactManagerAPITests.java
@@ -0,0 +1,13 @@
+package com.example.contactmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagerAPITests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
